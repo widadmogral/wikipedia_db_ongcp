41f61fa210b0490aea2c1d52191b7e8371d8f4ee
widadmogral@gmail.com <widadmogral@gmail.com>
2022-10-26 11:12:46 +0200

    removing unecessary try except and print statements

--- wikipedia_search.py
+++ wikipedia_search.py
@@ -4,6 +4,7 @@ wikipedia = MediaWiki()
 wiki_dict = {"title":'',"summary":'', "img_link":''}
 def wiki_get_summary_img(keyword):
         wiki_dict = {"title": '', "summary": '', "img_link": ''}
+
         try:
             p = wikipedia.page(keyword)
             wiki_dict["title"] = p.title
@@ -14,15 +15,12 @@ def wiki_get_summary_img(keyword):
                 wiki_dict["img_link"] = "No images for this keyword"
 
         except mediawiki.DisambiguationError as e:
-            #wiki_dict = None
             raise e
 
         except mediawiki.PageError as e:
-            #wiki_dict = None
             raise e
-            #wiki_dict["title"] = e
 
-        return None
+        return wiki_dict
 
 #def keyword_db_search(keyword):
 
